dbae30ef0793fda331cd45ecefc924bd1052ef85
NeoGlanding <neoglanding@protonmail.com>
2022-01-04 13:04:33 +0700

    clean version

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0
+            counter: 0,
+            counter2: 0
+        }
+    },
+    methods: {
+        addCounter2() {
+            this.counter2++
+        },
+        reduceCounter2() {
+            this.counter2--
         }
     }
 })
