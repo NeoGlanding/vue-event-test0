738d4ae02de0a344d3390828d1b7efcf1ea57d6c
NeoGlanding <neoglanding@protonmail.com>
2022-01-04 13:24:00 +0700

    working on input

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
-            counter2: 0
+            counter2: 0,
+            counter3: 0,
+            name: ''
         }
     },
     methods: {
-        addCounter2() {
-            this.counter2++
+        addCounter2(num) {
+            if (num) {
+                this.counter3 += num
+            } else {
+                this.counter2++
+            }
         },
-        reduceCounter2() {
-            this.counter2--
+        reduceCounter2(num) {
+            if (num) {
+                this.counter3 -= num
+            } else {
+                this.counter2--
+            }
+        },
+        setName(e) {
+            this.name = e.target.value
+        },
+        setName2(e, lastname) {
+            this.name = e.target.value + " " + lastname
         }
     }
 })
